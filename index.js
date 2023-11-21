37b6e7b62618e10c5ffc38fb41022ed7d67bf165
maicaocaa <maria.caocaa@gmail.com>
2023-11-21 22:53:39 +0100

    starting with js

---
+++ index.js
@@ -0,0 +1,24 @@
+
+let num1
+let num2
+
+    addition(num1, num2) {
+        return num1 + num2;
+    }
+
+    substraction (num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+
+    
+
+
+
