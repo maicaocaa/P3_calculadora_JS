7cd7a41cef98590d82224d4ae70bee3abd02ad27
maicaocaa <maria.caocaa@gmail.com>
2023-11-22 00:52:25 +0100

    html ok

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 
-let num1
-let num2
+let main_screen = document.querySelector(".main_screen");
+let calc_screen = document.querySelector(".calc_screen");
+calc_screen.innerHTML ="0";
+main_screen.innerHTML ="0";
 
-    addition(num1, num2) {
-        return num1 + num2;
-    }
 
-    substraction (num1, num2) {
-        return num1 - num2;
-    }
 
-    dividir(num1, num2) {
-        return num1 / num2;
-    }
+// class Calculadora {
+//     plus (num1, num2) {
+//         return num1 + num2;
+//     }
 
-    multiplicar(num1, num2) {
-        return num1 * num2;
-    }
+//     minus (num1, num2) {
+//         return num1 - num2;
+//     }
+
+//     divide (num1, num2) {
+//         return num1 / num2;
+//     }
+
+//     multiply (num1, num2) {
+//         return num1 * num2;
+//     }
 
     
-
+// } 
 
 
