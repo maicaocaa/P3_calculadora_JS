053de7641534aa419ea4b3f68e5cee4bbd862268
maicaocaa <maria.caocaa@gmail.com>
2023-11-23 11:20:59 +0100

    add new file

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
+//defino constantes para que objetengo de los id y clases del HTML
 
-let main_screen = document.querySelector(".main_screen");
-let calc_screen = document.querySelector(".calc_screen");
-calc_screen.innerHTML ="0";
-main_screen.innerHTML ="0";
-
-
-
-// class Calculadora {
-//     plus (num1, num2) {
-//         return num1 + num2;
-//     }
-
-//     minus (num1, num2) {
-//         return num1 - num2;
-//     }
-
-//     divide (num1, num2) {
-//         return num1 / num2;
-//     }
-
-//     multiply (num1, num2) {
-//         return num1 * num2;
-//     }
-
-    
-// } 
+const mainScreen = document.getElementById('main_screen');
+const calcScreen = document.getElementById('calc_screen');
+const numButton = document.querySelectorAll('.number');  // seleccina la clase
+const operatorButton = document.querySelectorAll ('.operator');
 
 
+// defino clase para funciones de calculo
+class Calculator {  
+         plus (num1, num2){
+            return num1 + num2;
+        };
+
+         minus (num1, num2){
+            return num1 - num2;
+        };
+         divide (num1, num2){
+            return num1 / num2;
+        };
+         multi (num1, num2){
+            return num1 * num2;
+        };
+         rest (num1, num2){
+            return num1 % num2;
+        };
+
+    };
+
+const calculator = new Calculator();
+
+
+console.log(calculator.plus(1, 2));
+// console.log(calculator.minus(1, 2));
+// console.log(calculator.divide(1, 2));
+// console.log(calculator.multi(1, 2));
+// console.log(calculator.rest(1, 2));
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", ()=> {
+    console.log ('presion boton')
+})
